c8033477a17cf5d280be0719bc24ada1f1f87d0f
6uiwj <oterges00@gmail.com>
2025-06-03 19:50:29 +0900

    250603 Truthy&Falsy

---
+++ section02/chapter01.js
@@ -0,0 +1,40 @@
+//truthy와 falsy
+//true와 false가 아니지만 true나 false처럼 취급되는 값
+
+//1.Falsy한 값(거짓)
+let f1 = undefined;
+let f2 = null;
+let f3 = 0;
+let f4 = -0;
+let f5 = NaN;
+let f6 = "";
+let f7 = 0n; //Big Integer
+
+if (!f1) {
+  console.log("false");
+}
+
+//2.Truthy한 값
+// -> 7가지 Falsy한 값들 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+if (t5) {
+  console.log("Truthy");
+}
+
+//3. 활용 사례
+function printName(person) {
+  if (!person) {
+    //falsy인가?
+    console.log("값없음");
+    return;
+  }
+  console.log(person.name);
+}
+
+let person = null;
+printName(person);
