11310e05d8598eb57d6fab2a2a4381a7afaf032a
molmj <minji@molcube.com>
2026-06-03 21:16:34 +0900

    react - state

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,30 +1,30 @@
 import './App.css';
-import Header from './components/Header'; //확장자 생략가능 
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Button from './components/Button';
+import {useState} from 'react';
 
 function App() {
 
-   const buttonProps = {
-      text: "메일",
-      color: "red",
-      a: 1,
-      b: 2,
-      c: 3,
-   };
-  
-  return (
+  //변수를 쓰지 않고 state를 사용하는 이유: 변수는 값이 바뀌어도 화면이 다시 렌더링되지 않지만, 
+  // state는 값이 바뀌면 화면이 다시 렌더링되기 때문에 
+  // 사용자에게 변경된 값을 보여줄 수 있다. 
+  // 따라서, 사용자 인터페이스에서 동적인 데이터를 관리할 때는 
+  // state를 사용하는 것이 적합하다.
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+  console.log(count);
+  return  (
     <>
-      <Header /> {/* header 컴포넌트 사용 */}
-      <Main /> {/* main 컴포넌트 사용 */}
-      <Footer /> {/* footer 컴포넌트 사용 */} 
-      <Button {...buttonProps} />
-      <Button text={"카페"}/>
-      <Button text={"블로그"}>
-        <div>자식 요소</div> {/* Button 컴포넌트의 children props로 자동으로 전달 */}
-        <Header /> {/* Button 컴포넌트의 children props로 자동으로 전달 */}
-        </Button>
+    <div>
+      <h1>{light }</h1>
+      <button onClick = {() => {
+        setLight(light === "ON" ? "OFF" : "ON");
+      }}> {light === "ON" ? "끄기" : "켜기"} </button>
+    </div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick= {() => 
+        setCount(count+1)
+      }>+</button>
+      </div>
     </>
   );
 }
